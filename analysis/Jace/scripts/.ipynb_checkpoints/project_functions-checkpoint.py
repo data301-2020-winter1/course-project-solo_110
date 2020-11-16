28ac845f91127be1cdb09a356853204b766eea84
Jace Lai <acejace@hotmail.com>
2020-11-15 23:19:34 -0800

    Completed Task 1 -> Task 3

--- analysis/Jace/scripts/.ipynb_checkpoints/project_functions-checkpoint.py
+++ analysis/Jace/scripts/.ipynb_checkpoints/project_functions-checkpoint.py
@@ -3,8 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def process():
-    print("magic is good")
 def load(path):
     df = pd.read_csv(path)
     return df
@@ -13,3 +11,16 @@ def load(path):
 def sort(dataset,column):
     df=dataset.sort_values(by=column,ascending=False)
     return df
+
+def countColumns(dataset):
+    count = dataset.shape[1]
+    return count
+
+def countRows(dataset):
+    count = dataset.shape[0]
+    return count
+
+def listColumns(dataset):
+    lst = dataset.columns
+    for item in lst:
+        print(item)
